aaf1fb1808a476f7808a7022665323887023bc98
siuying <siu.ying@gmail.com>
2011-07-01 01:04:02 +0800

    remove unnecessary code

--- app.rb
+++ app.rb
@@ -45,7 +45,7 @@ get '/' do
     #   You can then remove the following line, start using view templates, etc.
     "Hello, #{current_user.nickname}"
   else
-    '<a href="/sign_up">create an account</a> or <a href="/sign_in">sign in with Twitter</a>'
+    '<a href="/sign_up">create an account</a> or <a href="/auth/twitter">sign in with Twitter</a>'
     # if you replace the above line with the following line, 
     #   the user gets signed in automatically. Could be useful. 
     #   Could also break user expectations.
@@ -70,18 +70,7 @@ get "/auth/failure" do
   params[:message]
 end
 
-# any of the following routes should work to sign the user in: 
-#   /sign_up, /signup, /sign_in, /signin, /log_in, /login
-["/sign_in/?", "/signin/?", "/log_in/?", "/login/?", "/sign_up/?", "/signup/?"].each do |path|
-  get path do
-    redirect '/auth/twitter'
-  end
-end
-
-# either /log_out, /logout, /sign_out, or /signout will end the session and log the user out
-["/sign_out/?", "/signout/?", "/log_out/?", "/logout/?"].each do |path|
-  get path do
-    session[:user_id] = nil
-    redirect '/'
-  end
+get "/logout" do
+  session[:user_id] = nil
+  redirect '/'
 end
