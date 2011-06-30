ca61ea57a029b90365cee440b22811fc2a1b56ad
siuying <siu.ying@gmail.com>
2011-06-30 23:46:17 +0800

    revise message

--- app.rb
+++ app.rb
@@ -38,7 +38,7 @@ get '/' do
     # The following line just tests to see that it's working.
     #   If you've logged in your first user, '/' should load: "1 ... 1";
     #   You can then remove the following line, start using view templates, etc.
-    current_user.id.to_s + " ... " + session[:user_id].to_s 
+    "Hello, #{current_user.nickname}"
   else
     '<a href="/sign_up">create an account</a> or <a href="/sign_in">sign in with Twitter</a>'
     # if you replace the above line with the following line, 
@@ -58,6 +58,10 @@ get '/auth/:name/callback' do
 
   session[:user_id] = user.id
   redirect '/'
+end
+
+get "/auth/failure" do
+  params[:message]
 end
 
 # any of the following routes should work to sign the user in: 
